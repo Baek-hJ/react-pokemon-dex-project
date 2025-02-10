566775c6ebe6d9d926731d1746235572f9f1a589
hj <hyeonjubaek06@gmail.com>
2025-02-11 06:46:34 +0900

    Home 스타일링 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,52 @@
 // import React from 'react'
-
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
     const navigate = useNavigate()
     return (
-        <div>
-            <h1>Home 컴포넌트입니다</h1>
-            <button onClick={() => {
+        <Container>
+            <PokemonLogo src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/404px-International_Pok%C3%A9mon_logo.svg.png" alt="포켓몬 로고" />
+            <InBtn onClick={() => {
                 navigate("/dex")
             }}>
-                포켓몬 도감으로 이동
-            </button>
-        </div>
+            포켓몬 도감으로 이동
+            </InBtn>
+        </Container>
     )
-}
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url("https://i.pinimg.com/736x/77/d5/89/77d5892879682be6d71f83ff5eda71b4.jpg");
+  background-size: cover;
+  background-position: center;
+  padding: 3rem;
+
+`;
+
+const PokemonLogo = styled.img`
+    display: flex;
+    overflow: clip;
+    flex-direction: column;
+    margin: 9rem auto;
+`;
+
+const InBtn = styled.button`
+width: 20rem;
+height: 4rem;
+text-align: center;
+font-size: 1.5rem;
+font-weight: 700;
+background-color: rgb(240, 255, 255, 0.8);
+display: flex;
+flex-direction: column;
+margin: 5rem auto;
+padding: 10px;
+border-radius: 1rem;
+
+cursor: pointer;
+`;
 
 export default Home;
